7db284b0d57c24aae9b90fc377592fe45d1281d1
renate-hq <renate@hq.xyz>
2025-07-28 00:24:00 +0800

    Initializes Kiro Constellation extension
    Sets up the basic structure and functionality for the Kiro Constellation extension.
    
    This includes:
    - Adds a "Hello World" command for testing.
    - Configures extension activation and deactivation messages.
    - Updates dependencies and metadata.
    - Provides a basic README with usage instructions and development setup.

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,13 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "kiro-constellation" is now active!');
+	console.log('Kiro Constellation extension activated!');
+	
+	// Show a welcome message
+	vscode.window.showInformationMessage('Kiro Constellation extension activated!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -16,11 +19,13 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('kiro-constellation.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from kiro-Seeeeee!');
+		vscode.window.showInformationMessage('Hello World from kiro-constellation!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	console.log('Kiro Constellation extension deactivated');
+}
